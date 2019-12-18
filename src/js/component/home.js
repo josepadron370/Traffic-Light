478bfad0478f230d85b7370a8355347afb1fdc4d
josepadron370 <josepadron370@gmail.com>
2019-12-18 13:49:06 +0000

    lights get turned off when a new one is clicked

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,23 +5,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const turnOff = () => {
+		setLight1("light red off");
+		setLight2("light yellow off");
+		setLight3("light green off");
+	};
+
 	const [lightRed, setLight1] = useState("light red off");
-	var myVar = setInterval(myTimer, 10000);
-	function myTimer() {
-		var d = [changeGreen(), changeRed(), changeYellow()];
-		document.getElementById("auto").innerHTML = d;
-	}
 	const changeRed = () => {
 		if (lightRed === "light red off") {
 			setLight1("light red");
+			setLight2("light yellow off");
+			setLight3("light green off");
 		} else {
 			setLight1("light red off");
+			setLight2("light yellow off");
+			setLight3("light green off");
 		}
 	};
+
 	const [lightYellow, setLight2] = useState("light yellow off");
 	const changeYellow = () => {
 		if (lightYellow === "light yellow off") {
+			setLight1("light red off");
 			setLight2("light yellow");
+			setLight3("light green off");
 		} else {
 			setLight2("light yellow off");
 		}
@@ -29,6 +37,8 @@ export function Home() {
 	const [lightGreen, setLight3] = useState("light green off");
 	const changeGreen = () => {
 		if (lightGreen === "light green off") {
+			setLight1("light red off");
+			setLight2("light yellow off");
 			setLight3("light green");
 		} else {
 			setLight3("light green off");
@@ -47,14 +57,20 @@ export function Home() {
 			</div>
 			<div className="btn-group" role="group">
 				<button type="button" className="btn btn-success" id="auto">
-					On
+					Auto
 				</button>
-				<button type="button" className="btn btn-warning">
-					Click
-				</button>
-				<button type="button" className="btn btn-danger">
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={() => turnOff()}>
 					Off
 				</button>
+				{/* <button
+					type="button"
+					className="btn btn-warning"
+					onClick={() => turnOn()}>
+					On
+				</button> */}
 			</div>
 		</>
 	);
