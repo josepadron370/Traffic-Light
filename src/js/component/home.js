a69bca9b9171968e96a33a07e2673cf2b33b56a5
josepadron370 <josepadron370@gmail.com>
2019-12-18 14:12:50 +0000

    auto 1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,76 +1,88 @@
-import React, { useState } from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
-	const turnOff = () => {
+	const [light1, setLight1] = useState("light red off");
+	const [light2, setLight2] = useState("light yellow off");
+	const [light3, setLight3] = useState("light green off");
+	const [counter, setCounter] = useState(0);
+	const [auto, setAuto] = useState(false);
+	const changeRed = () => {
+		if (light1 === "light red off") {
+			setLight1("light red");
+		} else setLight1("light red off");
+	};
+
+	const changeYellow = () => {
+		if (light2 === "light yellow off") {
+			setLight2("light yellow");
+		} else setLight2("light yellow off");
+	};
+
+	const changeGreen = () => {
+		if (light3 === "light green off") {
+			setLight3("light green");
+		} else setLight3("light green off");
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setCounter(counter + 1);
+		}, 2000);
+		if (auto === true && counter === 1) {
+			setLight1("light red off");
+			setLight2("light yellow off");
+			setLight3("light green");
+		}
+		if (auto === true && counter === 8) {
+			setLight1("light red off");
+			setLight2("light yellow");
+			setLight3("light green off");
+		}
+		if (auto === true && counter === 11) {
+			setLight1("light red");
+			setLight2("light yellow off");
+			setLight3("light green off");
+		}
+		if (counter === 15) {
+			setCounter(0);
+		}
+	});
+
+	const lightOn = () => {
+		setAuto(true);
+	};
+
+	let lightOff = () => {
 		setLight1("light red off");
 		setLight2("light yellow off");
 		setLight3("light green off");
 	};
 
-	const [lightRed, setLight1] = useState("light red off");
-	const changeRed = () => {
-		if (lightRed === "light red off") {
-			setLight1("light red");
-			setLight2("light yellow off");
-			setLight3("light green off");
-		} else {
-			setLight1("light red off");
-			setLight2("light yellow off");
-			setLight3("light green off");
-		}
-	};
-
-	const [lightYellow, setLight2] = useState("light yellow off");
-	const changeYellow = () => {
-		if (lightYellow === "light yellow off") {
-			setLight1("light red off");
-			setLight2("light yellow");
-			setLight3("light green off");
-		} else {
-			setLight2("light yellow off");
-		}
-	};
-	const [lightGreen, setLight3] = useState("light green off");
-	const changeGreen = () => {
-		if (lightGreen === "light green off") {
-			setLight1("light red off");
-			setLight2("light yellow off");
-			setLight3("light green");
-		} else {
-			setLight3("light green off");
-		}
-	};
 	return (
 		<>
-			<div className="text-center mt-5">
-				<div className="toplight">
-					<div className="lightContainer">
-						<div className={lightRed} onClick={changeRed} />
-						<div className={lightYellow} onClick={changeYellow} />
-						<div className={lightGreen} onClick={changeGreen} />
-					</div>
-				</div>
+			<div className="stop-light-container">
+				<div className={light1} onClick={changeRed} />
+				<div className={light2} onClick={changeYellow} />
+				<div className={light3} onClick={changeGreen} />
 			</div>
-			<div className="btn-group" role="group">
-				<button type="button" className="btn btn-success" id="auto">
-					Auto
+			<div>{counter}</div>
+			<div className="buttons">
+				<button
+					type="button"
+					className="btn btn-light"
+					onClick={lightOn}>
+					On
+				</button>
+				<button type="button" className="btn btn-light">
+					click
 				</button>
 				<button
 					type="button"
-					className="btn btn-danger"
-					onClick={() => turnOff()}>
+					className="btn btn-light"
+					onClick={lightOff}>
 					Off
 				</button>
-				{/* <button
-					type="button"
-					className="btn btn-warning"
-					onClick={() => turnOn()}>
-					On
-				</button> */}
 			</div>
 		</>
 	);
